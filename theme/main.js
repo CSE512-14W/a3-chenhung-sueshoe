5c6c54cbdd55fbde46af7f77a5c12a1da965f129
chenhung <chenhung.wu@gmail.com>
2014-02-09 18:15:10 -0800

    Color / JS changed

--- theme/main.js
+++ theme/main.js
@@ -6,11 +6,26 @@ cr_main = {
     init: function(){
     }
 };
-
 $(".area").hover(function(){
     var districtcode = "." + $(this).attr("district");
-    $(districtcode).css({"opacity": 0.5});
+    $(districtcode).css("opacity", 1);
 }, function(){
     var districtcode = "." + $(this).attr("district");
-    $(districtcode).css({"opacity": 1});
+    $(districtcode).css("opacity", 0.1);
 });
+$("path").hover(function(){
+    var districtcode = "." + $(this).attr("district");
+    $(districtcode).css("opacity", 1);
+}, function(){
+    var districtcode = "." + $(this).attr("district");
+    $(districtcode).css("opacity", 0.1);
+});
+/*
+$(".area").hover(function(){
+    var districtcode = "." + $(this).attr("district");
+    $(districtcode).css("opacity", 1);
+}, function(){
+    var districtcode = "." + $(this).attr("district");
+    $(districtcode).css("opacity", 0.2);
+});
+*/
--- theme/t2_02091652.js
+++ theme/t2_02091652.js
@@ -1,12 +1,10 @@
 var m = [30, 10, 10, 10],
     w = 800 - m[1] - m[3],
-    h = 300 - m[0] - m[2];
+    h = 550 - m[0] - m[2];
 
 var x = d3.scale.ordinal().rangePoints([0, w], 1),
     y = {},
     dragging = {};
-
-var lowestpoint = h;
 
 var line = d3.svg.line(),
     axis = d3.svg.axis().orient("left"),
@@ -25,13 +23,19 @@ d3.csv("./data/crime.csv", function(crime) {
 
   // Extract the list of dimensions and create a scale for each.
   x.domain(dimensions = d3.keys(crime[0]).filter(function(d) {
-    return d != "District" && (y[d] = d3.scale.linear()
+    return d != "District" && d!= "Grand Total" && (y[d] = d3.scale.linear()
         .domain([0, d3.max(crime, function(p) { return +p[d]; })])
         .range([h, 0]));
   }));
 
-    console.log(crime);
-  crime.sort(function(a, b){return d3.ascending(a["Grand Total"], b["Grand Total"]);});
+  // Add grey background lines for context.
+  background = svg.append("svg:g")
+      .attr("class", "background")
+    .selectAll("path")
+      .data(crime)
+    .enter().append("svg:path")
+      .attr("d", path)
+      .attr("class", "path");
 
   // Add blue foreground lines for focus.
   foreground = svg.append("svg:g")
@@ -48,12 +52,12 @@ d3.csv("./data/crime.csv", function(crime) {
   var g = svg.selectAll(".dimension")
       .data(dimensions)
     .enter().append("svg:g")
-        .attr("class", "dimension")
-      .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
-  /*
+      .attr("class", "dimension")
+      .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
       .call(d3.behavior.drag()
         .on("dragstart", function(d) {
           dragging[d] = this.__origin__ = x(d);
+          background.attr("visibility", "hidden");
         })
         .on("drag", function(d) {
           dragging[d] = Math.min(w, Math.max(0, this.__origin__ += d3.event.dx));
@@ -68,8 +72,13 @@ d3.csv("./data/crime.csv", function(crime) {
           transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
           transition(foreground)
               .attr("d", path);
+          background
+              .attr("d", path)
+              .transition()
+              .delay(500)
+              .duration(0)
+              .attr("visibility", null);
         }));
-        */
 
   // Add an axis and title.
   g.append("svg:g")
@@ -80,16 +89,13 @@ d3.csv("./data/crime.csv", function(crime) {
       .attr("y", -9)
       .text(String);
 
-
   // Add and store a brush for each axis.
-  /*
   g.append("svg:g")
       .attr("class", "brush")
       .each(function(d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
     .selectAll("rect")
       .attr("x", -8)
       .attr("width", 16);
-      */
 });
 
 function position(d) {
@@ -103,15 +109,7 @@ function transition(g) {
 
 // Returns the path for a given data point.
 function path(d) {
-    var r = dimensions.map(function(p) {return [position(p), y[p](d[p])]});
-/*    r.push({(r[13][0]), 0});
-    r.push({(r[0][0]), 0});*/
-    var ra = [[r[13][0], h], [r[0][0], h]];
-    r.push(ra[0]);
-    r.push(ra[1]);
-    //console.log(r);
-    return line(r);
-//  return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+  return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
 }
 
 //Returns the classname for each path.
@@ -127,14 +125,14 @@ function districtcode(d) {
 
 
 // Handles a brush event, toggling the display of foreground lines.
-/*
 function brush() {
   var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
       extents = actives.map(function(p) { return y[p].brush.extent(); });
+  console.log(actives);
+  console.log(extents);
   foreground.style("display", function(d) {
     return actives.every(function(p, i) {
       return extents[i][0] <= d[p] && d[p] <= extents[i][1];
     }) ? null : "none";
   });
 }
-*/
