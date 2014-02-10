48cdbcbca716c6c2032abd61b0b7233904c170a9
chenhung <chenhung.wu@gmail.com>
2014-02-09 16:43:54 -0800

    Revert "Initializing_t2l"
    This reverts commit 5546a7a6ec760089128a631c873dbb07f3fed85d.

--- theme/main.js
+++ theme/main.js
@@ -1,16 +1,55 @@
-$(document).ready(function(){
-    cr_main.init();
-});
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-cr_main = {
-    init: function(){
+var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+        .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(20);
+
+var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+d3.csv("./data/crime.csv", type, function(error, data){
+        x.domain(data.map(function(d) {return d.District;}));
+        y.domain([0, d3.max(data, function(d) {return d.FRAUD})]);
+        svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+console.log(data);
+
+        svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar FRAUD")
+        .attr("x", function(d) { return x(d.District); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.FRAUD); })
+        .attr("height", function(d) { return height - y(d.FRAUD); });
+
     }
-};
+);
 
-$(".area").hover(function(){
-    var districtcode = "." + $(this).attr("district");
-    $(districtcode).css({"opacity": 0.5});
-}, function(){
-    var districtcode = "." + $(this).attr("district");
-    $(districtcode).css({"opacity": 1});
-});
+function type(d) {
+    d.FRAUD = +d.FRAUD;
+    d.BURGLARY = +d.BURGLARY;
+    d["BIKE THEFT"] = +d["BIKE THEFT"];
+    d["CAR PROWL"] = +d["CAR PROWL"];
+    return d;
+}
