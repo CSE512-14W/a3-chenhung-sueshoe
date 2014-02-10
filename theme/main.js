88a427c42d51b4b913ec03453d9cfb0c63a58ae5
chenhung <chenhung.wu@gmail.com>
2014-02-10 08:37:07 -0800

    Choropleth Map / Control Panel Added

--- theme/main.js
+++ theme/main.js
@@ -1,3 +1,5 @@
+var crimeclone = {};
+
 $(document).ready(function(){
     cr_main.init();
 });
@@ -10,10 +12,24 @@ $(".area").hover(function(){
     var districtname = $(this).attr("district");
     var districtcode = "." + districtname;
     $(districtcode).css("opacity", 1);
+    $("#seattlemap").find(districtcode).css("stroke-width", "2px");
     $("#localdistrict").html(districtname);
+    var val = findvalue(districtname);
+    console.log(val);
 }, function(){
     var districtname = $(this).attr("district");
     var districtcode = "." + districtname;
-    $(districtcode).css("opacity", 0.1);
+    $(districtcode).css("opacity", 0.05);
+    $("#seattlemap").find(districtcode).css("opacity", 0.5);
+    $("#seattlemap").find(districtcode).css("stroke-width", "0.5px");
     $("#localdistrict").html("Grand Total");
 });
+
+function findvalue(districtname){
+    for(var i = 0; i < crimeclone.length; ++i){
+        if(crimeclone[i]['District'] == districtname){
+            return crimeclone[i];
+            i = crimeclone.length;
+        }
+    }
+}
